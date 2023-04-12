67398dbd41f62ee2ca8e4e583bf116c932241866
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-04-12 15:47:45 +0200

    Primeros componentes creados, Estructura del componente Nuevo Presupuesto, relacion del estado entre Componente principal y secundarios.

---
+++ src/Components/NuevoPresupuesto.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import Mensaje from "./Mensaje"
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [ mensaje, setMensaje ] = useState("")
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        if(!Number(presupuesto) || Number(presupuesto)<0) {
+            setMensaje("no es un presupuesto valido")
+        } else {
+            setMensaje("si es un presupuesto valido")
+        }
+    }
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+        
+        <form onSubmit={handlePresupuesto} className="formulario">
+            <div className="campo">
+                <label>Definir Presupuesto</label>
+                <input
+                className="nuevo-presupuesto"
+                type="text"
+                placeholder="Añade tu presupuesto"
+                value={presupuesto}
+                onChange={(e) => {setPresupuesto(e.target.value)}}
+                />
+
+            </div>
+            <input type="submit" value="Añadir" />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        </form>
+    
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
